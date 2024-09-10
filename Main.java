a2e759964391ac279f5b875ac8ad49010061aa1d
amarjanthe <amarkallappa785@gmail.com>
2024-09-10 22:12:56 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+        System.out.println("Jenkins project we are using this code");
+    }
+};
