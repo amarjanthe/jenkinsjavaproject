d6031bfc54a58548f3022832c53dcac2fd88ed64
amarjanthe <amarkallappa785@gmail.com>
2024-09-11 00:23:26 +0530

    #2

--- Student.java
+++
@@ -1,8 +0,0 @@
-public class Student {
-    public Student(){
-        System.out.println(x:"creating student object");
-    }
-    public void sayHello() {
-        System.out.println("HI , I am Engineer Student");
-    }
-}
